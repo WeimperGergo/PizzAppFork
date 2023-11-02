d107342b1594257adcc2d92d36865b08e6e5aca5
WeimperGergo <weimper.geri@gmail.com>
2023-11-02 10:59:39 +0100

    vegleges verzio, minden mukodokepes.

--- src/pizzapp/PizzApp.java
+++ src/pizzapp/PizzApp.java
@@ -9,6 +9,7 @@ public class PizzApp extends javax.swing.JFrame {
     public int extrak;              // extra feltétek
     public int db;                  // Pizza darabszáma
     public double osszeg;           // Végső ár
+    public String pizzaOsszesites;
     
     int extra1 = 0;
     int extra2 = 0;
@@ -24,9 +25,7 @@ public class PizzApp extends javax.swing.JFrame {
         //int db = numDb.getValue();
         
         //if (numDb.getValue() > 0){}
-            
-        
-        
+
         osszegSzamolas(db);
         
     }
@@ -283,6 +282,8 @@ public class PizzApp extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void cmdValaszthatoPizzakActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdValaszthatoPizzakActionPerformed
+        // A combobox nevét elírtam, cmb helyett cmd-ként referálok rá ebben a kódban.
+        
         int pizzaIndex = cmdValaszthatoPizzak.getSelectedIndex();
         
         // (-1)-el jelöljük, hogy nincs ára a pizzának.
@@ -319,20 +320,24 @@ public class PizzApp extends javax.swing.JFrame {
         db = 1;
         
         osszegSzamolas(db);
+        osszesitesKiiras();
     }//GEN-LAST:event_cmdValaszthatoPizzakActionPerformed
 
     private void rdbMeret32ItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_rdbMeret32ItemStateChanged
         meretSzorzo = 1;
         osszegSzamolas(db);
+        osszesitesKiiras();
     }//GEN-LAST:event_rdbMeret32ItemStateChanged
 
     private void rdbMeret25ItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_rdbMeret25ItemStateChanged
         meretSzorzo = .75;
         osszegSzamolas(db);
+        osszesitesKiiras();
     }//GEN-LAST:event_rdbMeret25ItemStateChanged
 
     private void numDbStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_numDbStateChanged
         osszegSzamolas(db);
+        osszesitesKiiras();
     }//GEN-LAST:event_numDbStateChanged
 
     private void chbAnanaszItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_chbAnanaszItemStateChanged
@@ -344,7 +349,7 @@ public class PizzApp extends javax.swing.JFrame {
             extra1 = 0;
         }
         extrakSzamolas();
-        
+        osszesitesKiiras();
     }//GEN-LAST:event_chbAnanaszItemStateChanged
 
     private void chbHagymaItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_chbHagymaItemStateChanged
@@ -356,6 +361,7 @@ public class PizzApp extends javax.swing.JFrame {
             extra2 = 0;
         }
         extrakSzamolas();
+        osszesitesKiiras();
     }//GEN-LAST:event_chbHagymaItemStateChanged
 
     private void chbSajtItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_chbSajtItemStateChanged
@@ -367,10 +373,13 @@ public class PizzApp extends javax.swing.JFrame {
             extra3 = 0;
         }
         extrakSzamolas();
+        osszesitesKiiras();
     }//GEN-LAST:event_chbSajtItemStateChanged
 
     private void btnRendelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRendelActionPerformed
-        jOptSzoveg = "Köszönjük a rendelését! " + osszeg + " Ft lesz.";
+        int osszegSeged = (int) Math.round(osszeg);
+        
+        jOptSzoveg = "Köszönjük a rendelését! \n" + osszegSeged + " Ft lesz.";
         JOptionPane.showMessageDialog(rootPane, jOptSzoveg);
     }//GEN-LAST:event_btnRendelActionPerformed
 
@@ -448,5 +457,58 @@ public class PizzApp extends javax.swing.JFrame {
         extrak = extra1 + extra2 + extra3;
         osszegSzamolas(db);
     }
-            
+    
+    private void osszesitesKiiras() {
+        //pizzafajta
+        String cmbPizzaStr = cmdValaszthatoPizzak.getSelectedItem().toString();
+        
+        //pizzaméret
+        String meretStr = "";
+        if (rdbMeret25.isSelected() == true){
+            meretStr = "25 cm";
+        }
+        else if (rdbMeret32.isSelected() == true){
+            meretStr = "32 cm";
+        }
+        else meretStr = "";
+        
+        //extrák
+        String extrakStr = "";
+        String segedExtra = "";
+        if (chbAnanasz.isSelected() == true){
+            segedExtra = "ananász, ";
+            extrakStr += segedExtra;
+        }
+        else {
+            extrakStr = extrakStr.replace("ananász, ", "");
+        }
+        
+        if (chbHagyma.isSelected() == true){
+            segedExtra = "hagyma, ";
+            extrakStr += segedExtra;
+        }
+        else {
+            extrakStr = extrakStr.replace("hagyma, ", "");
+        }
+        
+        if (chbSajt.isSelected() == true){
+            segedExtra = "sajt, ";
+            extrakStr += segedExtra;
+        }
+        else {
+            extrakStr = extrakStr.replace("sajt, ", "");
+        }
+        
+        //darab
+        String dbStr = numDb.getValue().toString();
+        
+        //összeg
+        int osszegSeged = (int) Math.round(osszeg);
+        
+                
+        pizzaOsszesites = "" + cmbPizzaStr + ", " + meretStr + ", " + extrakStr + dbStr + "db\nÖsszesen: " + osszegSeged + " Ft.";
+        txaOsszesito.setText(pizzaOsszesites);
+    }
+    
 }
+
